513b860e3ff45ef7457067736a74c3977d758457
Khiem NV <nvkhiem2016@gmail.com>
2023-02-17 14:05:31 +0700

    add cors, custom port

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("port");
+  await app.listen(port);
+  Logger.log(`🚀 Listening at http://0.0.0.0:${port}`, "HTTP");
 }
 bootstrap();
